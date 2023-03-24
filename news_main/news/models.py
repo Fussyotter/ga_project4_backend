f913e30f765584861013f592e3475b723c1ffa0e
ialoysious <isaacaloysious@gmail.com>
2023-03-24 13:46:14 -0700

    fixing

--- news_main/news/models.py
+++ news_main/news/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Article(models.Model):
     author = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
@@ -9,5 +11,5 @@ class Article(models.Model):
     publishedAt = models.CharField(max_length=50)
     def __str__(self): return self.name
 
-# this is basic model for the v2/everything end point.  
+# this is basic model for the v2/everything end point.
 # probably need more models to get more specific but this is a start
