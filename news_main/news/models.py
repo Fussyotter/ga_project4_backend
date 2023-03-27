55db018ad510d944841d355848d10a6aab8c85ae
ialoysious <isaacaloysious@gmail.com>
2023-03-27 15:17:53 -0700

    rebasing

--- news_main/news/models.py
+++ news_main/news/models.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import User
 
 
 
+# this is basic model for the v2/everything end point.
+# probably need more models to get more specific but this is a start
+
+# basic template i'm testing
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=120)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+
 class Article(models.Model):
     author = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
