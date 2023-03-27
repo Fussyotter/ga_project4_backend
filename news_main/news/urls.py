cc39ee9b4d12b3fa031f12fccee9b4ae0b963f5c
ialoysious <isaacaloysious@gmail.com>
2023-03-27 16:18:58 -0700

    login trials

--- news_main/news/urls.py
+++ news_main/news/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
  
-    # path('login', my_view.as_view(), name='login' ),
+    path('login/', UserLoginView.as_view(), name='login' ),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('articles', views.ArticleList.as_view(), name='article_list'),
     path('articles/<int:pk>', views.ArticleDetail.as_view(), name='articles_detail'),
