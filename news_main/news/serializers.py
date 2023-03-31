08e63bdd0faae0e388d17bc552dd2c805ab85698
ialoysious <isaacaloysious@gmail.com>
2023-03-31 09:59:10 -0700

    updating serializers

--- news_main/news/serializers.py
+++ news_main/news/serializers.py
@@ -2,12 +2,22 @@ from rest_framework import serializers
 from .models import Article
 from django.contrib.auth.models import User
 
+
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Article
-        fields = ('id', 'author','title','description','url','publishedAt')
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['user'] = [str(user) for user in instance.user.all()]
+        return rep
+
+
 # testing this to see if I can keep similar format
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username','email','password','first_name','last_name')
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
